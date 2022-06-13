09d757a7e7ee196032be5709917b54458ecd7577
Jantay <jakcckaj12@gmail.com>
2022-06-13 19:31:24 +0600

    Конфликты с тикетом вроде решил

--- IdenTicket/IdenTicket/Models/Ticket.cs
+++ IdenTicket/IdenTicket/Models/Ticket.cs
@@ -10,9 +10,7 @@ namespace IdenTicket.Models
         public int Id { get; set; }
         public int FlightId { get; set; }
         public int CustomerId { get; set; }
-
         public ClassType ClassType { get; set; }
-        
         public virtual Flight Flight { get; set; }
         public virtual Customer Customer { get; set; }
     }
