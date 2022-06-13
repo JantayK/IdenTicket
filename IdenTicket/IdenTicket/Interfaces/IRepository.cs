823c148d203375d404d6d240fd4a05c3694079e4
Jantay <jakcckaj12@gmail.com>
2022-06-13 13:41:22 +0600

    Добавил IRepository и TicketRepository

---
+++ IdenTicket/IdenTicket/Interfaces/IRepository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdenTicket.Interfaces
+{
+    /// <summary>
+    /// Интерфейс Репозитория
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRepository<T> : IDisposable
+    {
+        /// <summary>
+        /// Получение всех обьектов
+        /// </summary>
+        /// <returns></returns>
+        IEnumerable<T> GetAll();
+
+        /// <summary>
+        /// Получение одного обьекта
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        T GetById(int id);
+
+        /// <summary>
+        /// Создание обьекта
+        /// </summary>
+        /// <param name="item"></param>
+        void Create(T item);
+
+        /// <summary>
+        /// Обновление обьекта
+        /// </summary>
+        /// <param name="item"></param>
+        void Update(T item);
+
+        /// <summary>
+        /// Удаление обьекта
+        /// </summary>
+        /// <param name="id"></param>
+        void Delete(int id);
+
+        /// <summary>
+        /// Сохранение изменений
+        /// </summary>
+        void Save();
+    }
+}
