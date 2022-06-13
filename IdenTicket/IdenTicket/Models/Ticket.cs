f0dd46326b21c1621dc9a54d8bd802b95c41298d
Jantay <jakcckaj12@gmail.com>
2022-06-13 19:39:35 +0600

    очень конфликтный тикет не хочет решаться

--- IdenTicket/IdenTicket/Models/Ticket.cs
+++ IdenTicket/IdenTicket/Models/Ticket.cs
@@ -10,7 +10,9 @@ namespace IdenTicket.Models
         public int Id { get; set; }
         public int FlightId { get; set; }
         public int CustomerId { get; set; }
+
         public ClassType ClassType { get; set; }
+
         public virtual Flight Flight { get; set; }
         public virtual Customer Customer { get; set; }
     }
