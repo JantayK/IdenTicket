5cb567ac6df8b06edbeb1bd1920e03bb096de753
Jantay <jakcckaj12@gmail.com>
2022-06-15 19:38:21 +0600

    Сделал вью модель поиска

---
+++ IdenTicket/IdenTicket/ViewModels/SearchViewModel.cs
@@ -0,0 +1,21 @@
+﻿using IdenTicket.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdenTicket.ViewModels
+{
+    /// <summary>
+    /// Вью Модель начальной страницы
+    /// </summary>
+    public class SearchViewModel
+    {
+        public string DepartureAirport { get; set; }
+        public string DestinationAirport { get; set; }
+        public DateTime DepartDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public FlightType FlightType { get; set; }
+    }
+}
