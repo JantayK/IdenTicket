5d67678d5134ce43342cd6ecfadad799b11af004
Mark Melnikov <melmarka4@gmail.com>
2022-06-19 16:33:56 +0600

    Сгенерировал миграцию для таблицы истории поиска

--- IdenTicket/IdenTicket/Data/Migrations/20220619103227_AddSearchHistory.Designer.cs
+++ IdenTicket/IdenTicket/Data/Migrations/20220619103227_AddSearchHistory.Designer.cs
@@ -4,14 +4,16 @@ using IdenTicket.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace IdenTicket.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220619103227_AddSearchHistory")]
+    partial class AddSearchHistory
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
